f68f528e2d9ba6e6bfeadf3a5426f815ebdf3ff9
Alexandria-dr <sasha110druz@gmail.com>
2021-04-24 22:06:02 +0300

    анимация кнопок, курсор для маленьких карточек с рисунками, паралакс эффект луны

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
+gsap.registerPlugin(ScrollTrigger);
+
 const navButton = document.querySelector('.header__burger');
 
+function init() {
 navButton.addEventListener('click', e => {navButton.classList.toggle('active');
 document.querySelector('.header__menu').classList.toggle('active');
 document.querySelector('body').classList.toggle('lock')})
+
+gsap.to('.img-moon',{
+    y: 500,
+    scrollTrigger: {
+        trigger:'.img-moon',
+        start:'top-=100 top+=150',
+        end:'bottom+=500 top',
+        scrub: true
+    }
+})
+}
+
+window.addEventListener('load', function () {
+    init();
+});
