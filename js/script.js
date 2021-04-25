23021e0fd00be23354ff7221de8eb52f5d6364b5
Alexandria-dr <sasha110druz@gmail.com>
2021-04-25 08:21:02 +0300

    анимация элементов блоков

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ gsap.registerPlugin(ScrollTrigger);
 
 const navButton = document.querySelector('.header__burger');
 
+const fromLeft = document.querySelectorAll('.gsap-from-left');
+const fromRight = document.querySelectorAll('.gsap-from-right')
+
 function init() {
 navButton.addEventListener('click', e => {navButton.classList.toggle('active');
 document.querySelector('.header__menu').classList.toggle('active');
@@ -11,10 +14,45 @@ gsap.to('.img-moon',{
     y: 500,
     scrollTrigger: {
         trigger:'.img-moon',
-        start:'top-=100 top+=150',
+        start:'top-=100 top+=100',
         end:'bottom+=500 top',
-        scrub: true
+        scrub: true,
+        markers: true,
     }
+})
+
+fromLeft.forEach(element => {
+    gsap.fromTo(element,{
+        x: -200,
+        opacity: 0},{
+        x: 0,
+        opacity: 1,
+        scrollTrigger:{
+            trigger:element,
+            start: 'top-=400 center',
+            end: 'top-=200 center-=50',
+            scrub: true,
+            // markers: true,
+        }
+        }
+    )
+})
+
+fromRight.forEach(element => {
+    gsap.fromTo(element,{
+        x: +200,
+        opacity: 0},{
+        x: 0,
+        opacity: 1,
+        scrollTrigger:{
+            trigger:element,
+            start: 'top-=400 center',
+            end: 'top-=200 center-=50',
+            scrub: true,
+            // markers: true,
+        }
+        }
+    )
 })
 }
 
