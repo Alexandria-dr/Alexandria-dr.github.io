3ae7b9ef3e00e576e2c1d16975de6dec2668d00b
Alexandria-dr <sasha110druz@gmail.com>
2021-04-27 13:33:04 +0300

    gsap

--- js/script.js
+++ js/script.js
@@ -6,12 +6,6 @@ const listFromLeft = document.querySelectorAll('.gsap-list-from-left');
 const fromLeft = document.querySelectorAll('.gsap-from-left');
 const fromRight = document.querySelectorAll('.gsap-from-right')
 
-var param = [
-    listFL = [listFromLeft, '-200', '-=280', '-=200', '-=50'  ],
-    fL = [fromLeft, '-200', '-=200', '-=0', ''],  
-    fR = [fromRight, '+200', fL[2], fL[3], fL[4]] ];
-
-
 function fromSide(for_, x, start, end1, end2 ) {
 for_.forEach(element => {
     gsap.from(element, {
@@ -22,6 +16,7 @@ for_.forEach(element => {
             start: `top${start} center`,
             end: `top${end1} center${end2}`,
             scrub: true,
+            markers: true
         }
     }
     )
@@ -29,6 +24,12 @@ for_.forEach(element => {
 }
 
 function initDisplay() {
+
+    var param = [
+        listFL = [listFromLeft, '-200', '-=280', '-=200', '-=50'  ],
+        fL = [fromLeft, '-200', '-=200', '-=0', ''],  
+        fR = [fromRight, '+200', fL[2], fL[3], fL[4]] ];
+
     gsap.to('.img-moon', {
         y: 500,
         scrollTrigger: {
@@ -50,6 +51,11 @@ function initDisplay() {
 }
 
 function initScreen() {
+
+    var param = [
+        listFL = [listFromLeft, '-200', '-=300', '-=180', '-=150'  ],
+        fL = [fromLeft, '-200', '-=100', '+=50', ''],  
+        fR = [fromRight, '+200', fL[2], fL[3], fL[4]] ];
 
     navButton.addEventListener('click', e => {
         navButton.classList.toggle('active');
