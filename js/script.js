328c59663bb99dd74f535eb8d6e02e197c4914e4
Alexandria-dr <79481637+Alexandria-dr@users.noreply.github.com>
2021-04-27 14:00:55 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -22,85 +22,69 @@ for_.forEach(element => {
 })
 }
 
+function moon(start) {
+    gsap.to('.img-moon', {
+        y: 500,
+        scrollTrigger: {
+            trigger: '.img-moon',
+            start: `top-=110 top+=${start}`,
+            end: 'bottom+=1500 top',
+            scrub: true,
+            markers: true
+        }
+    })
+    
+}
+
 function initDisplay() {
+    moon(100);
 
     var param = [
         listFL = [listFromLeft, '-200', '-=280', '-=200', '-=50'  ],
         fL = [fromLeft, '-200', '-=200', '-=0', ''],  
         fR = [fromRight, '+200', fL[2], fL[3], fL[4]] ];
 
-    gsap.to('.img-moon', {
-        y: 500,
-        scrollTrigger: {
-            trigger: '.img-moon',
-            start: 'top-=110 top+=100',
-            end: 'bottom+=1500 top',
-            scrub: true,
-            
-        }
-    })
-   
    for (let i = 0; i < 3; i++) {
        vvar = []
+
        for (let ii = 0; ii < 5; ii++) {
                 vvar.push(param[i][ii])
             }
+            
         fromSide.apply(this, vvar);
   }
 }
 
 function initScreen() {
-
-    var param = [
-        listFL = [listFromLeft, '-200', '-=300', '-=180', '-=150'  ],
-        fL = [fromLeft, '-200', '-=100', '+=50', ''],  
-        fR = [fromRight, '+200', fL[2], fL[3], fL[4]] ];
-
     navButton.addEventListener('click', e => {
         navButton.classList.toggle('active');
         document.querySelector('.header__menu').classList.toggle('active');
         document.querySelector('body').classList.toggle('lock')
     })
-
-    gsap.to('.img-moon', {
-        y: 500,
-        scrollTrigger: {
-            trigger: '.img-moon',
-            start: 'top-=110 top+=200',
-            end: 'bottom+=1500 top',
-            scrub: true,
-        }
-    })
+    
+    moon(200);
+    
+    var param = [
+        listFL = [listFromLeft, '-200', '-=300', '-=180', '-=150'  ],
+        fL = [fromLeft, '-200', '-=100', '+=50', ''],  
+        fR = [fromRight, '+200', fL[2], fL[3], fL[4]] ];
 
     for (let i = 0; i < 3; i++) {
         vvar = []
+
         for (let ii = 0; ii < 5; ii++) {
-                 vvar.push(param[i][ii])
+                vvar.push(param[i][ii])
              }
+
          vvar[1]= 0;
          fromSide.apply(this, vvar);
    }
-
 }
 
 const mm = window.matchMedia('(min-width: 768px)')
 
 window.addEventListener('load', function () {
 
-    // mm.matches ? init() : gsap.killTweensOf('*')
+  mm.matches ? initDisplay() : initScreen()
 
-    // if (mm.matches) {
-    //     initDisplay();
-    // } else {
-    //     initScreen();
-    // }
-
-
-    mm.matches ? initDisplay() : initScreen()
-
-    // if (mm.matches) {
-    //     initDisplay();
-    // }
-
-    // mm.matches && initDisplay()
 });
