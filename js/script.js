3d584206a3c265f5f175df0819ed4670bfed54b8
Alexandria-dr <sasha110druz@gmail.com>
2021-04-27 14:32:14 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -6,50 +6,45 @@ const listFromLeft = document.querySelectorAll('.gsap-list-from-left');
 const fromLeft = document.querySelectorAll('.gsap-from-left');
 const fromRight = document.querySelectorAll('.gsap-from-right')
 
-function fromSide(for_, x, start, end1, end2 ) {
-for_.forEach(element => {
-    gsap.from(element, {
-        x: x,
-        opacity: 0,
-        scrollTrigger: {
-            trigger: element,
-            start: `top${start} center`,
-            end: `top${end1} center${end2}`,
-            scrub: true,
-        }
-    }
-    )
-})
-}
-
-function moon(start) {
+function fromSide(for_, x, start, end1, end2, moon ) {
     gsap.to('.img-moon', {
         y: 500,
         scrollTrigger: {
             trigger: '.img-moon',
-            start: `top-=110 top+=${start}`,
+            start: `top-=110 top+=${moon}`,
             end: 'bottom+=1500 top',
             scrub: true,
         }
+    });
+
+    for_.forEach(element => {
+        gsap.from(element, {
+            x: x,
+            opacity: 0,
+            scrollTrigger: {
+                trigger: element,
+                start: `top${start} center`,
+                end: `top${end1} center${end2}`,
+                scrub: true,
+            }
+        }
+        )
     })
-    
 }
 
 function initDisplay() {
-    moon(100);
-
     var param = [
         listFL = [listFromLeft, '-200', '-=280', '-=200', '-=50'  ],
         fL = [fromLeft, '-200', '-=200', '-=0', ''],  
         fR = [fromRight, '+200', fL[2], fL[3], fL[4]] ];
 
    for (let i = 0; i < 3; i++) {
-       vvar = []
-
+       vvar = [];
        for (let ii = 0; ii < 5; ii++) {
                 vvar.push(param[i][ii])
             }
-            
+        vvar[5] = 100;
+        console.log(vvar);
         fromSide.apply(this, vvar);
   }
 }
@@ -61,21 +56,18 @@ function initScreen() {
         document.querySelector('body').classList.toggle('lock')
     })
     
-    moon(200);
-    
     var param = [
         listFL = [listFromLeft, '-200', '-=300', '-=180', '-=150'  ],
         fL = [fromLeft, '-200', '-=100', '+=50', ''],  
         fR = [fromRight, '+200', fL[2], fL[3], fL[4]] ];
 
     for (let i = 0; i < 3; i++) {
-        vvar = []
-
+        vvar = [];
         for (let ii = 0; ii < 5; ii++) {
                 vvar.push(param[i][ii])
              }
-
          vvar[1]= 0;
+         vvar[5] = 200;
          fromSide.apply(this, vvar);
    }
 }
